3dacaafb15216d638d1c9806aff7b899c3db74b0
Marcin Niemczyk <m_niemczyk@outlook.com>
2023-11-14 21:36:56 +0100

    track switching days

--- main.js
+++ main.js
@@ -1,11 +1,29 @@
 (async () => {
     const service = await import(browser.runtime.getURL("./service.js"));
-    const utils = await import(browser.runtime.getURL("./utils.js"));
 
-    const mealElements = document.getElementsByClassName("src__Box-sc-1sbtrzs-0 epuDbI");
-    const meals = Array.from(mealElements).filter(el => utils.isElementVisible(el));
+    const className = "src__Box-sc-1sbtrzs-0 epuDbI";
+    const parentClassName = "src__Box-sc-1sbtrzs-0 eVheZJ";
 
-    const summary = service.getDailySummary(meals);
-    console.log(summary);
+    const observerCallback = (mutationsList) => {
+        for (const mutation of mutationsList) {
+            if (mutation.type === 'childList') {
+                mutation.addedNodes.forEach(node => {
+                    if (node.classList && node.className === parentClassName) {
+                        const meals = Array.from(node.getElementsByClassName(className));
+                        const summary = service.getDailySummary(meals);
+                        console.log(summary)
+                    }
+                });
+            }
+        }
+    };
+
+    const observerOptions = {
+        childList: true,
+        subtree: true
+    };
+
+    const observer = new MutationObserver(observerCallback);
+    observer.observe(document.body, observerOptions);
 
 })();
--- utils.js
+++
@@ -1,9 +0,0 @@
-export function isElementVisible(element) {
-    if (element.style.display === 'none' || window.getComputedStyle(element).display === 'none') {
-        return false;
-    }
-    if (element.parentElement) {
-        return isElementVisible(element.parentElement);
-    }
-    return true;
-}
