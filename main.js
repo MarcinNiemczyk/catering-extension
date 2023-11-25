25c5f1af47b0f8ce9ede6625e8bc67b2de4f46a2
Marcin Niemczyk <m_niemczyk@outlook.com>
2023-11-25 12:01:38 +0100

    insert summary to html

--- main.js
+++ main.js
@@ -3,6 +3,8 @@
 
     const className = "src__Box-sc-1sbtrzs-0 epuDbI";
     const parentClassName = "src__Box-sc-1sbtrzs-0 eVheZJ";
+    const targetClass = "sc-jVKKsU kTAXMB";
+    const altTargetClass = "sc-jVKKsU jJckyL";
 
     const observerCallback = (mutationsList) => {
         for (const mutation of mutationsList) {
@@ -11,7 +13,13 @@
                     if (node.classList && node.className === parentClassName) {
                         const meals = Array.from(node.getElementsByClassName(className));
                         const summary = service.getDailySummary(meals);
-                        console.log(summary)
+                        let targetElement = document.getElementsByClassName(targetClass)[0];
+                        if (!targetElement) {
+                            targetElement = document.getElementsByClassName(altTargetClass)[0];
+                        }
+                        targetElement.innerHTML = summary;
+                        targetElement.style.margin = "20px 0";
+                        targetElement.style.height = "32px";
                     }
                 });
             }
